92e8dc67a4dc3a478e4aac9ebb232535b983540b
asarritzu-boop <a.sarritzu@gmail.com>
2026-02-14 19:19:49 +0100

    Refactor service worker for caching and activation

--- sw.js
+++ sw.js
@@ -2,19 +2,39 @@ const CACHE_NAME = 'cruci-bibbia-v1';
 const ASSETS = [
   './',
   './index.html',
-  './manifest.json',
   './icona-192.png',
-  './icona-512.png'
+  './icona-512.png',
+  './manifest.json'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+// Installazione del Service Worker e salvataggio dei file in cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(ASSETS);
+      })
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+// Attivazione e pulizia delle vecchie cache
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME)
+            .map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Gestione delle richieste: prova la cache, altrimenti vai in rete
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
   );
 });
